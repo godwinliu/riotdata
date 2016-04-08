5db1fd1604309cfc2d37d43f31a27add0ff1ae65
Godwin Liu <godwin.liu@gmail.com>
2016-04-08 18:38:00 -0400

    needs tidying but all functions working
     Changes to be committed:
    	modified:   lib/riotdata/riot_data_connector.rb

--- lib/riotdata/riot_data_connector.rb
+++ lib/riotdata/riot_data_connector.rb
@@ -8,6 +8,7 @@
 #  replaced by the ones here, in order to keep the library "DRY".
 
 require 'net/http'
+require 'tzinfo'
 
 module RiotDataConnector
   SERVER_URL = 'https://na.api.pvp.net'.freeze
@@ -20,15 +21,43 @@ module RiotDataConnector
   VERSION_PATH = '/v1.2/versions'.freeze
 
   CHAMP_IMAGE_PATH = '/img/champion'.freeze
+  DEFAULT_TZ = 'America/Toronto'
 
   $api_key = nil
+  $timezone = TZInfo::Timezone.get( DEFAULT_TZ )
+  $current_riot_version = nil
   
+  def self.api_key=( key )
+    $api_key = key
+  end
+
   def api_key=( key )
     $api_key = key
   end
 
+  def self.api_key?
+    $api_key ? true : false
+  end
+
   def api_key?
     $api_key ? true : false
+  end
+
+  def self.current_version
+    $current_riot_version ||= load_versions
+  end
+
+  def self.load_versions
+    # TODO - need to write such that this doesn't access an instance method
+    uri = static_uri( VERSION_PATH )
+    res = fetch_response(uri)
+    $versions = JSON.parse(res.body)
+    return $versions.first
+  end
+    
+  def self.static_uri( path, params = {} )
+    url = SERVER_URL + STATIC_DATA_PATH + REGION + path
+    return form_uri( url, params )
   end
 
   def static_uri( path, params = {} )
@@ -41,6 +70,13 @@ module RiotDataConnector
     return form_uri( url, params )
   end
   
+  def self.form_uri( url, params )
+    uri = URI( url )
+    unless p = merge_keyparam(params) then raise "bad data request param hash, or no api_key"; end
+    uri.query = URI.encode_www_form(p)
+    return uri
+  end
+
   def form_uri( url, params )
     uri = URI( url )
     unless p = merge_keyparam(params) then raise "bad data request param hash, or no api_key"; end
@@ -48,9 +84,23 @@ module RiotDataConnector
     return uri
   end
 
+  def self.merge_keyparam( h )
+    unless h.is_a?( Hash ) && api_key? then return nil; end
+    h.merge( {:api_key => $api_key} )
+  end
+
   def merge_keyparam( h )
     unless h.is_a?( Hash ) && api_key? then return nil; end
     h.merge( {:api_key => $api_key} )
+  end
+
+  def self.fetch_response( uri, log = false )
+    unless uri.is_a?( URI::HTTPS ) then raise "bad uri for data request"; end
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      puts "\n\tFetching URI: #{uri.to_s}" if log
+      http.get(uri.request_uri)
+      # TODO - implement some error handling here
   end
 
   def fetch_response( uri, log = false )
@@ -59,7 +109,6 @@ module RiotDataConnector
       http.use_ssl = true
       puts "\n\tFetching URI: #{uri.to_s}" if log
       http.get(uri.request_uri)
-      # TODO - implement some error handling here
   end
 
 end
